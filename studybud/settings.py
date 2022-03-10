49cb037e6db8b09db418b725d0d34b626af7267a
anjesa <anjesat@outlook.com>
2022-03-10 02:50:55 -0500

    test

--- studybud/settings.py
+++ studybud/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'profiles',
     # ------
     'allauth',
@@ -143,7 +144,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 django_heroku.settings(locals())
 
 # Additional login config settings, source: https://dev.to/mdrhmn/django-google-authentication-using-django-allauth-18f8
-SITE_ID = 2
+SITE_ID = 1
 LOGIN_REDIRECT_URL = '/profiles'
 
 SOCIALACCOUNT_QUERY_EMAIL = True
