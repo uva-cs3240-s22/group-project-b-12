8b40df3b39152f63906bd0d200e21ed410ae326f
Neha Pattanaik <81281204+nehapattanaik@users.noreply.github.com>
2022-03-27 16:10:06 -0400

    Update tests.py
    adding setup

--- tests.py
+++ tests.py
@@ -8,8 +8,10 @@ from django.urls import reverse
 from django.test import TestCase 
 
 class SessionTestCase(TestCase): 
-    def test_fields(self):
+    def setup(self): 
         Session.objects.create(attendees = 1, date='2022-03-12 07:30', location='Alderman', course='CS 3240', details = 'testing')
-        self.assertEqual(Session.__str__(self),"Study session with 1 at Alderman on March 12, 2022, 7:30 p.m. for CS 3240. Here are any additional details: testing.")
+    def test_fields(self):
+        test1 = Session.objects.get(course='CS 3240')
+        self.assertEqual(Session.__str__(test1),"Study session with 1 at Alderman on March 12, 2022, 7:30 p.m. for CS 3240. Here are any additional details: testing.")
 
 
