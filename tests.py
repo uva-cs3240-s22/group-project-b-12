0e91056109cde8ba90f260371c7420fe4a750132
Neha Pattanaik <81281204+nehapattanaik@users.noreply.github.com>
2022-03-27 15:28:12 -0400

    Update tests.py
    added test class

--- tests.py
+++ tests.py
@@ -7,9 +7,9 @@ from django.urls import reverse
 
 from django.test import TestCase 
 
-
-def setup(self):
-    Session.objects.create(attendees=4, date='2/4/2022', location='Alderman', course='CS 3240', details = 'testing')
-    self.assertEqual(Session.__str__(),"Study session with 4 at Alderman on 2/4/2022 for CS 3240. Here are any additional details: testing.")
+class SessionTestCase(TestCase): 
+    def test_fields(self):
+        Session.objects.create(attendees=4, date='2/4/2022', location='Alderman', course='CS 3240', details = 'testing')
+        self.assertEqual(Session.__str__(),"Study session with 4 at Alderman on 2/4/2022 for CS 3240. Here are any additional details: testing.")
 
 
