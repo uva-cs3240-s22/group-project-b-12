c41c47bf312ca437ece70d5f08c0666394e21d2d
Neha Pattanaik <81281204+nehapattanaik@users.noreply.github.com>
2022-03-27 16:20:07 -0400

    Update tests.py
    change filter to get

--- tests.py
+++ tests.py
@@ -11,7 +11,7 @@ class SessionTestCase(TestCase):
     def setup(self): 
         Session.objects.create(date='2022-03-12 07:30', location='Alderman', course='CS 3240', details = 'testing')
     def test_fields(self):
-        test1 = Session.objects.filter(course='CS 3240')
+        test1 = Session.objects.get(course='CS 3240')
         self.assertEqual(Session.__str__(test1),"Study session at Alderman on March 12, 2022, 7:30 p.m. for CS 3240. Here are any additional details: testing.")
 
 
