7f749807a835891349637dda3aac8a98401d051c
Neha Pattanaik <81281204+nehapattanaik@users.noreply.github.com>
2022-03-27 15:30:43 -0400

    Update tests.py
    fixing invalid date format

--- tests.py
+++ tests.py
@@ -10,6 +10,6 @@ from django.test import TestCase
 class SessionTestCase(TestCase): 
     def test_fields(self):
         Session.objects.create(attendees=4, date='2/4/2022', location='Alderman', course='CS 3240', details = 'testing')
-        self.assertEqual(Session.__str__(),"Study session with 4 at Alderman on 2/4/2022 for CS 3240. Here are any additional details: testing.")
+        self.assertEqual(Session.__str__(),"Study session with 4 at Alderman on 2022-04-02 00:00 for CS 3240. Here are any additional details: testing.")
 
 
