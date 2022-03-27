ece817e22b20b872297bfca7d121e2433ddc97ab
nesa7 <69175848+nesa7@users.noreply.github.com>
2022-03-27 03:06:41 -0400

    Revert "Merge pull request #7 from uva-cs3240-s22/Hyperlink"
    This reverts commit 289c39d5979671c33f937a8507fc246ab3e5edbe.

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-#3/20 ONLY one goes to loginView
+
 urlpatterns = [
     path('', views.loginView, name="loginView"),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.shortcuts import redirect
 
 # Create your views here.
 def loginView(request):
-    # if request.user.is_authenticated:
-    #     return redirect("/sessions")
     return render(request, 'profiles/login.html', {})
