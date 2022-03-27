ee386f6934d0f29c8cc7826b676e2876b3488a71
Neha Pattanaik <81281204+nehapattanaik@users.noreply.github.com>
2022-03-27 15:57:28 -0400

    Update tests.py
    remove attendees

--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ from django.test import TestCase
 
 class SessionTestCase(TestCase): 
     def test_fields(self):
-        Session.objects.create(attendees='4', date='2022-03-12 07:30', location='Alderman', course='CS 3240', details = 'testing')
-        self.assertEqual(Session.__str__(self),"Study session with 4 at Alderman on March 12, 2022, 7:30 p.m. for CS 3240. Here are any additional details: testing.")
+        Session.objects.create(date='2022-03-12 07:30', location='Alderman', course='CS 3240', details = 'testing')
+        self.assertEqual(Session.__str__(self),"Study session with 1 at Alderman on March 12, 2022, 7:30 p.m. for CS 3240. Here are any additional details: testing.")
 
 
