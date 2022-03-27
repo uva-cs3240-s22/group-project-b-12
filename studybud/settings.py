29a9feb292ee4efda73d7a3d08563cd0c3b0dcb5
anjesa <anjesat@outlook.com>
2022-03-27 05:03:04 -0400

    remove bootstrap from installed apps

--- studybud/settings.py
+++ studybud/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap5',
     'studybud.apps.StudybudConfig',
     'django.contrib.admin',
     'django.contrib.auth',
