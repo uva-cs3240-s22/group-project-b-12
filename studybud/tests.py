f881e534b4624fd0a624687ceb300f93f2b6be6c
vladratiu <98709859+vladratiu@users.noreply.github.com>
2022-03-27 16:41:13 -0400

    Update tests.py

--- studybud/tests.py
+++ studybud/tests.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from studybud.models import Session
+from .models import Session
 from django.utils import timezone
 from django.views import generic
 from django.urls import reverse
@@ -9,7 +9,8 @@ from django.test import TestCase
 
 class SessionTestCase(TestCase): 
     def setup(self): 
-        Session.objects.create(date='2022-03-12 07:30', location='Alderman', course='CS 3240', details = 'testing')
+        session = Session.objects.create(date='2022-03-12 07:30', location='Alderman', course='CS 3240', details = 'testing')
+        session.save()
     #def test_fields(self):
      #   test1 = Session.objects.get(course='CS 3240')
       #  self.assertEqual(Session.__str__(test1),"Study session at Alderman on March 12, 2022, 7:30 p.m. for CS 3240. Here are any additional details: testing.")
