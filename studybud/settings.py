7299cb59f6c81db03fe212e0ec7315e5ea0128bb
anjesa <anjesat@outlook.com>
2022-03-27 05:00:39 -0400

    add to installed apps

--- studybud/settings.py
+++ studybud/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap5',
+    'studybud.apps.StudybudConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
