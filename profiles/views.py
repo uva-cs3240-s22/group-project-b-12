a2af638d00c62a2ca1a1b3c978d1b5701c04013a
vladratiu <98709859+vladratiu@users.noreply.github.com>
2022-04-02 19:44:59 -0400

    Update views.py
    Added views for profile and their updates

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render, redirect
+from profiles.models import Profile
+from django.contrib.auth.models import User
+from django.views import generic
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def loginView(request):
@@ -6,5 +11,34 @@ def loginView(request):
         return redirect("/sessions")
     return render(request, 'profiles/login.html', {})
 
-def profileView(request):
-    return render(request, 'profiles/profile.html', {})
+
+class profileView(generic.ListView):
+    template_name='profiles/profile.html'
+    context_object_name = 'profile_list'
+    #session_list = Session.objects.all()
+                                    #objects.filter(date__gte=timezone.now())
+
+    def get_queryset(self):
+        return Profile.objects.all().last()
+
+def updateProfile(request):
+    if request.method == "POST":
+        profile = Profile.objects.create(username = request.POST['Username'], about = request.POST['Bio'], year = request.POST['Year'])
+
+        return HttpResponseRedirect(reverse('profilepage'))
+    else:
+        return render(request, 'profiles/profile.html', {'error': 'method is not post'} )
+
+
+class updateProfileView(generic.ListView):
+    template_name = 'profiles/updateProfile.html'
+    context_object_name = 'profile_list'
+
+    profile_list = Profile.objects.all()
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Profile.objects.all()
