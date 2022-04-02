af14529181e55ca8d8eb2186499ca2b3befff7b0
vladratiu <98709859+vladratiu@users.noreply.github.com>
2022-04-01 20:43:36 -0400

    Update views.py
    Added profile model to views

--- profiles/views.py
+++ profiles/views.py
@@ -5,3 +5,6 @@ def loginView(request):
     if request.user.is_authenticated:
         return redirect("/sessions")
     return render(request, 'profiles/login.html', {})
+
+def profileView(request):
+    return render(request, 'profiles/profile.html', {})
