06069ce68210ea8c441389a1fa8d4278e9de283c
vladratiu <98709859+vladratiu@users.noreply.github.com>
2022-04-02 19:44:25 -0400

    Update urls.py
    Added more paths for profile updates

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,5 +4,7 @@ from . import views
 #3/20 ONLY one goes to loginView
 urlpatterns = [
     path('', views.loginView, name="loginView"),
-    path('profilepage', views.profileView, name="profileView")
+    path('profilepage', views.profileView.as_view(), name="profileView"),
+    path('profileupdate', views.updateProfile, name="update"),
+    path('profileview', views.updateProfileView.as_view(), name="viewUpdate")
 ]
