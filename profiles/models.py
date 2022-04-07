54b61198f28ce7a6c74d460d561c799e5f7661d2
vladratiu <98709859+vladratiu@users.noreply.github.com>
2022-04-07 01:34:31 -0400

    Update models.py
    Testing the removal of the model of profiles

--- profiles/models.py
+++ profiles/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+###
 # Create your models here.
-class Profile(models.Model):
+# class Profile(models.Model):
     #user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+##    user = models.OneToOneField(User, on_delete=models.CASCADE)
     #Creates username for user
     #username = models.CharField(max_length = 100)
 
     #User can input some information about themselves
-    about = models.TextField(max_length = 250)
+##    about = models.TextField(max_length = 250)
 
     #User can indicate what year they are in
-    year = models.IntegerField(default = 1)
+##    year = models.IntegerField(default = 1)
 
-    def __str__(self):
-        return self.user.username
+##    def __str__(self):
+##        return self.user.username
