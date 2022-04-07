ebaed92cdbd5732039de1054da7e959541b9f757
vladratiu <98709859+vladratiu@users.noreply.github.com>
2022-04-07 01:38:10 -0400

    Update models.py

--- profiles/models.py
+++ profiles/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 ###
 # Create your models here.
-# class Profile(models.Model):
+class Profile(models.Model):
     #user = models.OneToOneField(User, on_delete=models.CASCADE)
-##    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     #Creates username for user
     #username = models.CharField(max_length = 100)
 
@@ -14,5 +14,5 @@ from django.contrib.auth.models import User
     #User can indicate what year they are in
 ##    year = models.IntegerField(default = 1)
 
-##    def __str__(self):
-##        return self.user.username
+    def __str__(self):
+        return self.user
