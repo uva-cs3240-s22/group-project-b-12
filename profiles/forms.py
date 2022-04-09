a4ff53fcde282c0da075fb8ed32ead517d16d26b
vladratiu <98709859+vladratiu@users.noreply.github.com>
2022-04-09 16:26:58 -0400

    Update forms.py

--- profiles/forms.py
+++ profiles/forms.py
@@ -50,3 +50,17 @@ class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['bio']
+        
+        
+
+class UpdateUserForm(forms.ModelForm):
+    username = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(required=True,
+                             widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
