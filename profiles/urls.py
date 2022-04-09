f83ca8d348b12cfeb497de8960c25f2933e0a339
vladratiu <98709859+vladratiu@users.noreply.github.com>
2022-04-09 13:53:38 -0400

    Update urls.py

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from .views import RegisterView
+
 #3/20 ONLY one goes to loginView
 urlpatterns = [
     path('', views.loginView, name="loginView"),
-    path('logout', views.logoutView, name="logoutView")
+    #path('register/', RegisterView.as_view(), name ='user-register'),
+    path('logout', views.logoutView, name="logoutView"),
+    path('register', RegisterView.as_view(), name='user-register'),
+    path('profile/', views.profile, name="users-profile"),
+    #path('profile', views.profile, name="users-profile")
 ]
