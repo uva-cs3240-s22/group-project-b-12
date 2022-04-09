e01e9a1909d81b23d7a5e7be2c6144c5fba62256
vladratiu <98709859+vladratiu@users.noreply.github.com>
2022-04-09 16:51:29 -0400

    Update apps.py

--- profiles/apps.py
+++ profiles/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class ProfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'profiles'
+    
+class UserConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'users'
+
+    def ready(self):
+        import users.signals  # noqa
