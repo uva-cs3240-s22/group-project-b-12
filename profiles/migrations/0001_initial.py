328e5d27a96bd640d7c4c0799d9816387240f0de
vladratiu <98709859+vladratiu@users.noreply.github.com>
2022-04-09 16:58:37 -0400

    Update 0001_initial.py

--- profiles/migrations/0001_initial.py
+++ profiles/migrations/0001_initial.py
@@ -17,7 +17,6 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Profile',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('bio', models.TextField()),
                 ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
