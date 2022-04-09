7f127991c77fe93cd35c2ed7b38e2588003cc473
vladratiu <98709859+vladratiu@users.noreply.github.com>
2022-04-09 16:03:12 -0400

    Update models.py

--- profiles/models.py
+++ profiles/models.py
@@ -4,15 +4,20 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     #user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    #user = models.OneToOneField(User, on_delete=models.CASCADE)
     #Creates username for user
     #username = models.CharField(max_length = 100)
 
     #User can input some information about themselves
-    about = models.TextField(max_length = 250)
+    #about = models.TextField(max_length = 250)
 
     #User can indicate what year they are in
-    year = models.IntegerField(default = 1)
+    #year = models.IntegerField(default = 1)
+
+# Extending User Model Using a One-To-One Link
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    bio = models.TextField()
 
     def __str__(self):
-        return self.user
+        return self.user.username
