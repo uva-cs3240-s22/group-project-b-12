2763b8b916aa08fe88b716ccdd65364039666fbb
Neha Pattanaik <81281204+nehapattanaik@users.noreply.github.com>
2022-04-10 18:33:13 -0400

    Update models.py

--- profiles/models.py
+++ profiles/models.py
@@ -14,3 +14,20 @@ class Profile(models.Model):
 class Test(models.Model):
     test = models.TextField()
 # Create your models here.
+class Courses(models.Model):
+    subject= models.CharField(max_length=50, blank = True, null = True)
+    catalog_number = models.CharField(max_length=10, blank = True, null = True)
+    class_section = models.CharField(max_length=4000, blank = True, null = True)
+    class_number = models.CharField(max_length=20, blank = True, null = True)
+    class_title = models.CharField(max_length=20, blank = True, null = True)
+    class_topic_formal_desc = models.CharField( max_length=50, blank = True, null = True)
+    instructor = models.CharField(max_length=50, blank = True, null = True)
+    enrollment_capacity = models.CharField(max_length=50, blank = True, null = True)
+    meeting_days = models.CharField(max_length=50, blank = True, null = True)
+    meeting_time_start = models.CharField(max_length=50, blank = True, null = True)
+    meeting_time_end = models.CharField(max_length=50, blank = True, null = True)
+    term = models.CharField(max_length=50, blank = True, null = True)
+    term_desc = models.CharField(max_length=50, blank = True, null = True)
+
+    def __str__(self):
+        return self.subject + self.catalog_number
