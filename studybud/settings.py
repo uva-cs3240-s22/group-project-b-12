751344d8992a3a501af83e570878eb9e8f554c64
anjesa <anjesat@outlook.com>
2022-04-16 15:46:14 -0400

    uncomment heroku settings

--- studybud/settings.py
+++ studybud/settings.py
@@ -150,9 +150,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #UNCOMMENT THIS when deploying to heroku
 #TODO: Ask TA how to not have to do this everytime we deploy
-# if '/app' in os.environ['HOME']:
-#     import django_heroku
-#     django_heroku.settings(locals())
+if '/app' in os.environ['HOME']:
+    import django_heroku
+    django_heroku.settings(locals())
 
 # Additional login config settings, source: https://dev.to/mdrhmn/django-google-authentication-using-django-allauth-18f8
 SITE_ID = 1
