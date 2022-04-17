c9b744a16f6630e9e9fc95d254a6366f916e2805
anjesa <anjesat@outlook.com>
2022-04-17 15:51:49 -0400

    increase char length for courses model fields

--- profiles/models.py
+++ profiles/models.py
@@ -15,19 +15,19 @@ class Test(models.Model):
     test = models.TextField()
 # Create your models here.
 class Courses(models.Model):
-    subject= models.CharField(max_length=50, blank = True, null = True)
-    catalog_number = models.CharField(max_length=10, blank = True, null = True)
-    class_section = models.CharField(max_length=4000, blank = True, null = True)
-    class_number = models.CharField(max_length=20, blank = True, null = True)
-    class_title = models.CharField(max_length=20, blank = True, null = True)
-    class_topic_formal_desc = models.CharField( max_length=50, blank = True, null = True)
-    instructor = models.CharField(max_length=50, blank = True, null = True)
-    enrollment_capacity = models.CharField(max_length=50, blank = True, null = True)
-    meeting_days = models.CharField(max_length=50, blank = True, null = True)
-    meeting_time_start = models.CharField(max_length=50, blank = True, null = True)
-    meeting_time_end = models.CharField(max_length=50, blank = True, null = True)
-    term = models.CharField(max_length=50, blank = True, null = True)
-    term_desc = models.CharField(max_length=50, blank = True, null = True)
+    subject= models.CharField(max_length=60, blank = True, null = True)
+    catalog_number = models.CharField(max_length=60, blank = True, null = True)
+    class_section = models.CharField(max_length=60, blank = True, null = True)
+    class_number = models.CharField(max_length=60, blank = True, null = True)
+    class_title = models.CharField(max_length=60, blank = True, null = True)
+    class_topic_formal_desc = models.CharField( max_length=60, blank = True, null = True)
+    instructor = models.CharField(max_length=60, blank = True, null = True)
+    enrollment_capacity = models.CharField(max_length=60, blank = True, null = True)
+    meeting_days = models.CharField(max_length=60, blank = True, null = True)
+    meeting_time_start = models.CharField(max_length=60, blank = True, null = True)
+    meeting_time_end = models.CharField(max_length=60, blank = True, null = True)
+    term = models.CharField(max_length=60, blank = True, null = True)
+    term_desc = models.CharField(max_length=60, blank = True, null = True)
 
 # New course model because we don't need more field
 class Course(models.Model):
